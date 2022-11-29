d2cda529064b959384733ed0931e48ba8283ab2a
Josh Archer <jarcher@greenriver.edu>
2022-11-29 11:41:49 -0800

    Added initial files.

---
+++ src/test/java/com/example/circlecipractice/CircleCiPracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.circlecipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiPracticeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
